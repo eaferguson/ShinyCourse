882258dead59c40ee05c262ffb7b1f5c1123a4ab
Rachel Steenson <r.steenson@hotmail.co.uk>
2019-06-05 12:58:36 +0100

    Text added to app 2

--- Apps/Day 1 - timeseries_1/ui.R
+++ Apps/Day 1 - timeseries_1/ui.R
@@ -12,13 +12,20 @@ library(ggplot2)
 raw_data <- read.csv("data/raw_data.csv", stringsAsFactors=FALSE)
 
 # Collect a list of regions for the dropdown menu
-regions_list <- c("All", sort(unique(raw_data$region)))
+regions_list <- c("All Regions", sort(unique(raw_data$region)))
 
 # Define UI for application that draws a histogram
 shinyUI(fluidPage(
 
   # Application title
   titlePanel("Exploratory plots: Timeseries"),
+
+  # Add a line break
+  br(),
+
+  # Add text section
+  h4("This app is a little more complicated than the first one."),
+  h4("There is still only 1 widget (selectInput), but this time we are changing the region we want to view on the plot."),
 
   # Add a line break
   br(),
