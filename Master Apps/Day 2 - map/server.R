ed1348313241ef5056281709ac6e2bb1775c9480
Elaine Ferguson <elaine.ferguson@glasgow.ac.uk>
2019-06-13 12:03:41 +0100

    Add colour by age and date

--- Master Apps/Day 2 - map/server.R
+++ Master Apps/Day 2 - map/server.R
@@ -10,8 +10,13 @@ library(RColorBrewer)
 # Load in the raw data
 raw_data <- read.csv("data/raw_data.csv", stringsAsFactors=FALSE)
 
+# Add year and decimal date to data
+leaflet_data <- raw_data %>% 
+  mutate(year = substr(date, 1,4), date=ymd(date), date_decimal = decimal_date(date)) 
+
 # # Create a colour palette
 # col_palette <- c("#231D51", "#178B8B", "#63C963", "#FFE31D")
+colour_pal <- brewer.pal(11, "Spectral")
 
 ## Load region shapefile
 regions <- readOGR("data/TZ_Region_2012","TZ_Region_2012")
@@ -20,29 +25,30 @@ regions <- readOGR("data/TZ_Region_2012","TZ_Region_2012")
 # Define server logic required to draw a histogram
 shinyServer(function(input, output) {
   
-  # # Species type: colorFactor() creates a discrete colour palette for leaflet to use
-  colour_pal <- brewer.pal(11, "Spectral")
   
-  
-  # Get colours based on chosen variable
+  # Get point colours based on chosen variable
   pal <- reactive({
-    colorFactor(colour_pal, domain = sort(unique(raw_data[,input$colourby])))
-  })
-  colours <- reactive({
-    pal()(raw_data[,input$colourby])
+    colourby <- ifelse(input$colourby!="date",input$colourby,"date_decimal")
+    if(is.character(leaflet_data[,colourby])){
+      colorFactor(colour_pal, domain = sort(unique(leaflet_data[,colourby])))  
+    }else{
+      colorNumeric(colour_pal, range(leaflet_data[,colourby]))
+    }
   })
   
   
   ## Render map
   output$mymap <- renderLeaflet({
+    colourby <- ifelse(input$colourby!="date",input$colourby,"date_decimal")
     leaflet() %>%
       addPolygons(data=regions,color="black",fillColor = "white", weight=1, fillOpacity=0.6) %>%
-      addCircles(data=raw_data,lng=~raw_data$x,lat=~raw_data$y,
-                 color = colours(),
+      addCircles(data=leaflet_data,lng=~leaflet_data$x,lat=~leaflet_data$y,
+                 color = pal()(leaflet_data[,colourby]),
                  opacity=1, fillOpacity=1) %>%
       addProviderTiles("Stamen.Terrain") %>%
       addLegend(position = "bottomright", title = input$colourby,
-                pal = pal(), values = raw_data[,input$colourby], opacity=1)
+                pal = pal(), values = leaflet_data[,colourby], opacity=1,
+                labFormat = labelFormat(big.mark = ""))
   })
   
 })
--- Master Apps/Day 2 - map/ui.R
+++ Master Apps/Day 2 - map/ui.R
@@ -6,13 +6,12 @@
 library(shiny)
 library(leaflet)
 
-# Define UI for application that draws a histogram
 shinyUI(fluidPage(
 
   leafletOutput("mymap",width=800,height=600),
   
   selectInput(inputId="colourby", label="Colour Cases By:",
-              choices = c("species","sex"),
+              choices = c("species","date","sex","age"),
               selected="species")
   
   
