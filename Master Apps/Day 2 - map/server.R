86bab2efe29c9867b17c5383800ee3061cccd273
Elaine Ferguson <elaine.ferguson@glasgow.ac.uk>
2019-06-13 17:17:41 +0100

    Add popups/hover elements and preliminary raster code

--- Master Apps/Day 2 - map/server.R
+++ Master Apps/Day 2 - map/server.R
@@ -6,6 +6,10 @@
 library(shiny)
 library(leaflet)
 library(RColorBrewer)
+library(lubridate)
+library(dplyr)
+library(rgdal)
+library(raster)
 
 # Load in the raw data
 raw_data <- read.csv("data/raw_data.csv", stringsAsFactors=FALSE)
@@ -14,15 +18,26 @@ raw_data <- read.csv("data/raw_data.csv", stringsAsFactors=FALSE)
 leaflet_data <- raw_data %>% 
   mutate(year = substr(date, 1,4), date=ymd(date), date_decimal = decimal_date(date)) 
 
-# # Create a colour palette
-# col_palette <- c("#231D51", "#178B8B", "#63C963", "#FFE31D")
-colour_pal <- brewer.pal(11, "Spectral")
+# # Create a colour palette for points
+point_palette <- c("#231D51", "#178B8B", "#63C963", "#FFE31D")
+# point_palette <- brewer.pal(11, "Spectral")
+
 
 ## Load region shapefile
 regions <- readOGR("data/TZ_Region_2012","TZ_Region_2012")
 
 
-# Define server logic required to draw a histogram
+## Load density raster
+density <- raster("data/HumanDensity/HumanPopulation.grd")
+
+
+## Colour palette for rasters
+raster_palette <- brewer.pal(9, "YlOrRd")
+
+
+
+
+# Define server logic 
 shinyServer(function(input, output) {
   
   
@@ -32,29 +47,47 @@ shinyServer(function(input, output) {
                          is.element(leaflet_data$species,input$species)),]
   })
   
+  popupInfo <- reactive({
+    paste("Date: ", leaflet_data_sub()$date, "<br>",
+          "Species: ", leaflet_data_sub()$species, "<br>",
+          "Age: ", leaflet_data_sub()$age, "<br>",
+          "Sex: ", leaflet_data_sub()$sex, "<br>",
+          sep = " ")
+  })
+   
+  
   # Get point colours based on chosen variable
   pal <- reactive({
     colourby <- ifelse(input$colourby!="date",input$colourby,"date_decimal")
     if(is.character(leaflet_data[,colourby])){
-      colorFactor(colour_pal, domain = sort(unique(leaflet_data[,colourby])))  
+      colorFactor(point_palette, domain = sort(unique(leaflet_data[,colourby])))  
     }else{
-      colorNumeric(colour_pal, range(leaflet_data[,colourby]))
+      colorNumeric(point_palette, range(leaflet_data[,colourby]))
     }
   })
   
   
   ## Render map
   output$mymap <- renderLeaflet({
+    rasterRange <- range(log10(density[])[which(!is.infinite(log10(density[])))],na.rm=T)
     colourby <- ifelse(input$colourby!="date",input$colourby,"date_decimal")
     leaflet() %>%
-      addPolygons(data=regions,color="black",fillColor = "white", weight=1, fillOpacity=0.6) %>%
+      # addPolygons(data=regions,color="black",fillColor = "white", 
+      #             label=regions$Region_Nam, weight=1, fillOpacity=0.9) %>%
       addCircles(data=leaflet_data_sub(),lng=~leaflet_data_sub()$x,lat=~leaflet_data_sub()$y,
                  color = pal()(leaflet_data_sub()[,colourby]),
-                 opacity=1, fillOpacity=1) %>%
+                 opacity=1, fillOpacity=1, popup = popupInfo()) %>%
       addProviderTiles("Stamen.Terrain") %>%
+      addRasterImage(log10(density), 
+                     colors = colorNumeric(raster_palette, rasterRange, na.color = "transparent"), 
+                     opacity = 0.7) %>% 
       addLegend(position = "bottomright", title = input$colourby,
                 pal = pal(), values = leaflet_data[,colourby], opacity=1,
-                labFormat = labelFormat(big.mark = ""))
+                labFormat = labelFormat(big.mark = "")) %>%
+      addLegend(pal = colorNumeric(raster_palette, rasterRange, na.color = "transparent"), 
+                values = rasterRange, opacity=1,bins = -10:10,labels=as.character((-10:10)^10),
+                title = "Log10 human<br>population<br>density")
+    
   })
   
 })
