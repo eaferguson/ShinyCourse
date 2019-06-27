29b4ad12e5aeef91f31e52299052dfece67eadea
Rachel Steenson <r.steenson@hotmail.co.uk>
2019-06-27 17:30:11 +0100

    Update to day 1 intro to shiny course stuff

--- Apps/Day 1 - barplot_1/server.R
+++ Apps/Day 1 - barplot_1/server.R
@@ -4,32 +4,36 @@
 # application by clicking 'Run App' above.
 # ---------------------------------------------------------------------------- #
 
+# Load in the libraries
 library(shiny)
 library(dplyr)
 library(ggplot2)
 library(RColorBrewer)
 
+# Load in the data
 raw_data <- read.csv("data/raw_data.csv", stringsAsFactors=FALSE)
 
 # Create a colour palette
-#col_palette <- c("#231D51", "#178B8B", "#63C963", "#FFE31D")
 col_palette <- brewer.pal(name="Dark2", n=8)
 
 #------------------------------------------------------------------------------#
-# Define server logic required to draw a histogram
+# Open the shiny section
 shinyServer(function(input, output) {
 
   # Produce plot
   output$barPlot <- renderPlot({
     ggplot() +
-      geom_bar(data=raw_data, aes(x=raw_data[[input$xaxis]]), fill=col_palette[1]) +
-      labs(x=paste0("\n", input$xaxis), y="Number of records\n") +
-      theme_classic() +
-      theme(axis.text = element_text(size=14),
+      geom_bar(data=raw_data, # set the dataset used
+               aes(x=raw_data[[input$xaxis]]), # use the input from the ui to give the column name for the x-axis
+               fill=col_palette[1]) + # use the first colour from the colour palette
+      labs(x=paste0(input$xaxis), y="Number of records") + # Set the axis labels
+      theme_classic() + # use a simple theme (no gridlines)
+      theme(axis.text = element_text(size=14), # make all plot text bigger (axes, titles, labels)
             axis.title = element_text(size=18),
             plot.title = element_text(size=20),
             legend.title = element_text(size=18),
             legend.text = element_text(size=14))
-  })
 
-})
+    }) # Close the renderPlot
+
+}) # Close the shinyServer
--- Apps/Day 1 - barplot_1/ui.R
+++ Apps/Day 1 - barplot_1/ui.R
@@ -4,9 +4,10 @@
 # run the application by clicking 'Run App' above.
 # ---------------------------------------------------------------------------- #
 
+# Load in libraries
 library(shiny)
 
-# Define UI for application that draws a histogram
+# Open the ui section
 shinyUI(fluidPage(
 
   # Application title
@@ -22,21 +23,27 @@ shinyUI(fluidPage(
   # Add a line break
   br(),
 
-  # Add content to the Sidebar
+  # Create a sidebar layout
   sidebarLayout(
+
+    # Add content to the Sidebar
     sidebarPanel(
-      selectInput(inputID="xaxis", # input id name
+
+      # Add a dropdown menu
+      selectInput(inputId="xaxis", # input id name
                   label = h3("Select the x-axis variable:"), # input title
                   choices = list("Sex" = "sex", "Species" = "species"), # choices that appear in the dropdown menu matched to the variable in the data
                   selected = 1) # number stating which of the choices starts as being selected
 
-    ),
+    ), # Close the sidebarPanel - remember the comma because the mainPanel is within this section
 
     # Add content to the main panel
     mainPanel(
       plotOutput("barPlot", # output id name
                  height=700) # height in pixels of the plot
-    )
-  )
 
-))
+    ) # Close the mainPanel - no comma needed now as we are closing the section
+
+  ) # Close the sidebarLayout
+
+)) # Close the fluidPage and the ShinyUI
