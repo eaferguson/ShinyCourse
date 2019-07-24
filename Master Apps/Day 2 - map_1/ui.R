a93f5e1c5b16921354ba4301c0cec7a8b374a60a
Elaine Ferguson <elaine.ferguson@glasgow.ac.uk>
2019-07-24 22:02:25 +0100

    map_1 app

---
+++ Master Apps/Day 2 - map_1/ui.R
@@ -0,0 +1,62 @@
+# ---------------------------------------------------------------------------- #
+# Day 2 - map Master App
+# This is the user-interface definition of a Shiny web application. You can
+# run the application by clicking 'Run App' above.
+# ---------------------------------------------------------------------------- #
+
+# Load libraries
+library(shiny)
+library(leaflet)
+library(shinyWidgets)
+library(dplyr)
+library(lubridate)
+
+# Load in the raw data
+raw_data <- read.csv("data/raw_data.csv", stringsAsFactors=FALSE)
+
+# Tranform dates from characters to date objects
+leaflet_data <- raw_data %>% mutate(date=ymd(date)) 
+
+# Get the unique names of the species for the drop down menu
+all_species <- unique(leaflet_data$species)
+
+
+#------------------------------------------------------------------------------#
+# Define UI for application 
+shinyUI(fluidPage(
+  
+  # Application title
+  titlePanel("Interactive map"),
+  
+  # Add a line break
+  br(),
+  
+  # Add text section
+  h4("This app allows spatial exploration of the data."),
+  h4("The widgets allow selection of the data points we want to view on the map, and colouring of the points based on different variables."),
+  
+  # Add a line break
+  br(),
+  
+  sidebarLayout(
+    
+    # Sidebar containing the widgets
+    sidebarPanel(
+      
+      # Drop down menu to choose variable by which points will be coloured
+      selectInput(inputId="colourby", label="Colour Cases By:",
+                  choices = c("species","date","sex","age"),
+                  selected="species")
+      
+    ),
+    
+    
+    # Show a plot of the map
+    mainPanel(
+      leafletOutput("mymap",width=1000,height=700)
+    )
+    
+  )
+  
+
+))
