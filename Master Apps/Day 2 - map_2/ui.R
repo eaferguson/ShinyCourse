a7707336f7d342fdc90644f603fd0c08d4cad803
Elaine Ferguson <elaine.ferguson@glasgow.ac.uk>
2019-07-29 11:26:12 +0100

    Fix commenting

--- Master Apps/Day 2 - map_2/ui.R
+++ Master Apps/Day 2 - map_2/ui.R
@@ -51,7 +51,7 @@ shinyUI(fluidPage(
       
       br(),
       
-      # Menu for selecting which species to display
+      # Menu for selecting which region to display
       pickerInput(inputId = "region", label = "Region:",
                   sort(all_regions), selected= all_regions, # Use sort to get names in alphabetical order
                   options = list(`actions-box` = TRUE,`live-search` = TRUE), multiple = T)
