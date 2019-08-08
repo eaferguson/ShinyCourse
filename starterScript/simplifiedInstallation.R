3b78b28677121111caf4ad01412a2e346bf88068
eaferguson <e.ferguson.2@research.gla.ac.uk>
2019-08-08 07:58:06 +0100

    Create simplifiedInstallation.R

---
+++ starterScript/simplifiedInstallation.R
@@ -0,0 +1,37 @@
+#
+## Packages to install
+#__________________________________________
+
+
+## Day 1
+install.packages("dplyr", dependencies = T); library("dplyr")
+install.packages("ggplot2", dependencies = T); library("ggplot2")
+install.packages("magrittr", dependencies = T); library("magrittr")
+install.packages("shiny", dependencies = T); library("shiny")
+install.packages("lubridate", dependencies = T); library("lubridate")
+install.packages("RColorBrewer", dependencies = T); library("RColorBrewer")
+
+
+## Day 2
+install.packages("raster", dependencies = T); library("raster")
+install.packages("rgeos", dependencies = T); library("rgeos")
+install.packages("mapview", dependencies = T); library("mapview")
+install.packages("rgdal", dependencies = T); library("rgdal")
+install.packages("leaflet", dependencies = T); library("leaflet")
+install.packages("shinyWidgets", dependencies = T); library("shinyWidgets")
+
+
+## Less essential but useful
+install.packages("htmltools", dependencies = T); library("htmltools")
+install.packages("kableExtra", dependencies = T); library("kableExtra")
+install.packages("knitr", dependencies = T); library("knitr")
+install.packages("pillar", dependencies = T); library("pillar")
+install.packages("plotly", dependencies = T); library("plotly")
+install.packages("png", dependencies = T); library("png")
+install.packages("rlang", dependencies = T); library("rlang")
+install.packages("rmarkdown", dependencies = T); library("rmarkdown")
+install.packages("sf", dependencies = T); library("sf")
+install.packages("shinydashboard", dependencies = T); library("shinydashboard")
+install.packages("vctrs", dependencies = T); library("vctrs")
+install.packages("Xmisc", dependencies = T); library("Xmisc")
+install.packages("devtools", dependencies = T); library("devtools")
